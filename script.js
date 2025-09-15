7c796b3922c38b439d2ad4d64154db4d3ed54037
unknown <iflisaad17@gmail.com>
2025-09-15 10:43:19 +0100

    Ark Days

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-document.querySelector(".Download").addEventListener("click", ()=>{
-    alert("test");
+document.querySelector(".cta").addEventListener("click", ()=>{
+    alert("Thank you for subscribing ");
 })
 
+var input = document.querySelector(".content");
+var check = document.getElementById("valid");
+var press = document.querySelector(".qr_button")
+
+
+console.log(input)
+console.log(check)
+
+input.addEventListener('input', () => {
+  if (input.value.trim() === "") {
+    check.style.color = "red";
+    check.style.display = "block";
+  }
+});
+press.addEventListener('click',()=>{
+    if (input.value.trim() === ""){
+        check.style.color = "red"
+        check.style.display = "block"
+        check.style.paddingTop = "15px"
+    }
+})
 
